f693a8c65afadd568bc621867623c91884a3c0e7
INPNOfficer <yuki-2000@mineo.jp>
2017-11-19 18:26:08 +0900

    base

--- src/main/java/info/inpn/INPNMod/INPNMod.java
+++ src/main/java/info/inpn/INPNMod/INPNMod.java
@@ -22,45 +22,24 @@ public class INPNMod
 */
 package info.inpn.INPNMod;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Metadata;
+import net.minecraftforge.fml.common.ModMetadata;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = INPNMod.MODID, version = INPNMod.VERSION,name = INPNMod.MODNAME, useMetadata = true)
 public class INPNMod
 {
 
-    /*
-    @ObjectHolder(MODID)
-    public static class BLOCKS{
-        public static final Block core = null;
-    }
-
-    @ObjectHolder(MODID)
-    public static class ITEMS{
-        public static final Item core = null;
-    }
-
-
-    //アイテム登録用イベント
-    @SubscribeEvent
-    protected static void registerItems(RegistryEvent.Register<Item> event){
-        event.getRegistry().registerAll(
-                new ItemBlock(BLOCKS.core).setRegistryName(MODID, "コ↑ア下")
-        );
-    }
-
-    //ブロック登録用イベント
-    @SubscribeEvent
-    protected static void registerBlocks(RegistryEvent.Register<Block> event){
-        event.getRegistry().registerAll(
-                new Block(Material.ROCK)
-                        .setRegistryName(MODID, "ココア･コアcore")//登録名の設定
-                        .setCreativeTab(CreativeTabs.BUILDING_BLOCKS)//クリエイティブタブの選択
-                        .setUnlocalizedName("core")//システム名の設定
-                        .setHardness(1.5F)//硬さ
-                        .setResistance(1.0F)//爆破耐性
-        );
-    }
-    @EventHandler
+	public static final String MODID = "inpnmod";
+	public static final String VERSION = "β";
+	public static final String MODNAME = "大日本ペイント帝国海軍MOD";
+	@Metadata(MODID)
+	public static ModMetadata meta;
+	@EventHandler
     public void preInit(FMLPreInitializationEvent event){
-        if (event.getSide().isClient())
-            ModelLoader.setCustomModelResourceLocation(ITEMS.core, 0, new ModelResourceLocation(ITEMS.core.getRegistryName(), "inventory"));
+		//Infoファイルを読み込む
+		Modinfo.loadInfo(meta);
     }
-    */
 }
